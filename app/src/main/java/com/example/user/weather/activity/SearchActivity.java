42e1a8f5b6756778c38eeb7e2f9abb86accaec72
YujiKoyano <yuji.lkoyano.800@gmail.com>
2016-01-30 00:16:50 +0900

    地名検索Topのactivityを追加

---
+++ app/src/main/java/com/example/user/weather/activity/SearchActivity.java
@@ -0,0 +1,54 @@
+package com.example.user.weather.activity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.SearchView;
+
+import com.example.user.weather.R;
+import com.example.user.weather.databinding.ActivitySearchBinding;
+import com.example.user.weather.fragment.SearchFragment;
+
+public class SearchActivity extends ActivityBase {
+
+    private static final String TAG = SearchActivity.class.getSimpleName();
+
+    private ActivitySearchBinding binding;
+
+    private SearchView searchView;
+
+    private SearchFragment searchFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.fragment_container, new SearchFragment(), SearchFragment.TAG);
+            transaction.commit();
+        }
+
+        binding.toolBar.inflateMenu(R.menu.search);
+        searchView = (SearchView) binding.toolBar.getMenu().findItem(R.id.menu_search).getActionView();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
